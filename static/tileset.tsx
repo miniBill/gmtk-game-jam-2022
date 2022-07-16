13ea83c10d661997e03f3ec0d4745fff934b50ff
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2022-07-16 19:34:25 +0200

    Smarter build

---
+++ static/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="tileset" tilewidth="16" tileheight="16" tilecount="33" columns="11">
+ <image source="tileset.png" width="176" height="48"/>
+</tileset>
